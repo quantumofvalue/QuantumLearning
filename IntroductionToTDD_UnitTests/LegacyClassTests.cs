6a2ddda12f5de0f568eaf0c90b2116fafe365b8d
Marcin Czenko <marcin@everydayproductive.com>
2012-12-06 01:32:55 +0100

    Added failed test and NUnit package.

--- IntroductionToTDD_UnitTests/LegacyClassTests.cs
+++ IntroductionToTDD_UnitTests/LegacyClassTests.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using NUnit.Framework;
+
 namespace IntroductionToTDD_UnitTests
 {
+    [TestFixture]
     public class LegacyClassTests
     {
+        [Test]
+        public void DummyTest()
+        {
+            Assert.Fail("I am supposed to fail!");
+        }
     }
 }
