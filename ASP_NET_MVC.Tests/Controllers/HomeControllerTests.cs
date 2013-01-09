f2da6453d426e0067fb73e945a5b5f02cceec529
Marcin Czenko <marcin@everydayproductive.com>
2013-01-09 10:26:25 +0100

    Added ItemRepository and some additional unit tests.

--- ASP_NET_MVC.Tests/Controllers/HomeControllerTests.cs
+++ ASP_NET_MVC.Tests/Controllers/HomeControllerTests.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using System.Web.Routing;
 using ASP_NET_MVC;
 using ASP_NET_MVC.Controllers;
+using ASP_NET_MVC.Models;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rhino.Mocks;
@@ -16,12 +17,12 @@ namespace ASP_NET_MVC.Tests.Controllers
     [TestClass]
     public class HomeControllerTests
     {
-        private static HttpContextBase GetMockHttpContext(string path)
+        private static HttpContextBase GetHttpContextMock(string path)
         {
             var context = MockRepository.GenerateStub<HttpContextBase>();
             context.Stub(x => x.Request).Return(MockRepository.GenerateStub<HttpRequestBase>());
             context.Request.Stub(x => x.PathInfo).Return("");
-            context.Request.Stub(x => x.AppRelativeCurrentExecutionFilePath).Return("~/");
+            context.Request.Stub(x => x.AppRelativeCurrentExecutionFilePath).Return(path);
             return context;
         }
 
@@ -38,7 +39,7 @@ namespace ASP_NET_MVC.Tests.Controllers
         public void DeafulatRouteIsConnectedToTheHomeControllerIndexAction()
         {
             // Arrange
-            var context = GetMockHttpContext("~/");
+            var context = GetHttpContextMock("~/");
             var routes = new RouteCollection();
             RouteConfig.RegisterRoutes(routes);
 
@@ -50,14 +51,82 @@ namespace ASP_NET_MVC.Tests.Controllers
         }
 
         [TestMethod]
+        public void HomeControllerCreateActionIsCorrectlyConnected()
+        {
+            // Arrange
+            var context = GetHttpContextMock("~/Home/Create");
+            var routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+
+            // Act
+            RouteData routeData = routes.GetRouteData(context);
+
+            // Assert
+            AssertRouteData(routeData, "Home", "Create", "");
+        }
+
+        [TestMethod]
         public void IndexActionRendersIndexView()
         {
-            HomeController homeController = new HomeController();
+            MockRepository mocks = new MockRepository();
+
+            ItemRepository itemRepositoryMock = mocks.StrictMock<ItemRepository>();
+
+            HomeController homeController = new HomeController(itemRepositoryMock);
 
             ViewResult result = homeController.Index() as ViewResult;
 
             Assert.IsNotNull(result);
             Assert.AreEqual("Index", result.ViewName);
+        }
+
+        [TestMethod]
+        public void IndexActionProvidesTheItemsToTheView()
+        {
+            MockRepository mocks = new MockRepository();
+            ItemRepository itemRepositoryMock = mocks.StrictMock<ItemRepository>();
+
+            List<Item> expectedItems = new List<Item>();
+            expectedItems.Add(new Item { ID = 1, ItemText = "Item1" });
+            expectedItems.Add(new Item { ID = 2, ItemText = "Item2" });
+
+            using (mocks.Record())
+            {
+                Expect.Call(itemRepositoryMock.GetAllItems()).Return(expectedItems);
+            }
+
+            using (mocks.Playback())
+            {
+                HomeController homeController = new HomeController(itemRepositoryMock);
+                ViewResult result = homeController.Index() as ViewResult;
+
+                IEnumerable<Item> actualItems = (IEnumerable<Item>)result.ViewData.Model;
+                Assert.IsNotNull(actualItems);
+                CollectionAssert.AreEqual(actualItems.ToList(), expectedItems);
+            }
+        }
+
+        [TestMethod]
+        public void CreateActionCreatesNewItem()
+        {
+            MockRepository mocks = new MockRepository();
+            ItemRepository itemRepositoryMock = mocks.StrictMock<ItemRepository>();
+
+            Item item = new Item { ID = 1, ItemText = "Item1" };
+
+            using (mocks.Record())
+            {
+                Expect.Call(delegate { itemRepositoryMock.CreateNewItem(item); });
+            }
+
+            using (mocks.Playback())
+            {
+                HomeController homeController = new HomeController(itemRepositoryMock);
+                RedirectToRouteResult redirectResult = homeController.Create(item) as RedirectToRouteResult;
+                Assert.IsNotNull(redirectResult);
+                Console.Write(redirectResult.RouteValues);
+                Assert.AreEqual("Index", redirectResult.RouteValues["Action"]);
+            }
         }
     }
 }
--- ASP_NET_MVC/Controllers/HomeController.cs
+++ ASP_NET_MVC/Controllers/HomeController.cs
@@ -4,16 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using ASP_NET_MVC.Models;
+
 namespace ASP_NET_MVC.Controllers
 {
     public class HomeController : Controller
     {
+        ItemRepository _itemRepository;
+
+        public HomeController(ItemRepository itemRepository)
+        {
+            _itemRepository = itemRepository;
+        }
         //
         // GET: /Home/
 
         public ActionResult Index()
         {
-            return View("Index");
+            return View("Index",_itemRepository.GetAllItems());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            _itemRepository.CreateNewItem(item);
+            return RedirectToAction("Index");
         }
     }
 }
