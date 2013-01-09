ffedb28b9303d8d54add6da10b63f8bfe149d9d1
Marcin Czenko <marcin@everydayproductive.com>
2013-01-09 10:27:18 +0100

    Extending the previous commit - the actual model and repository classes.

---
+++ ASP_NET_MVC/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_NET_MVC.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string ItemText { get; set; }
+    }
+}
---
+++ ASP_NET_MVC/Models/ItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_NET_MVC.Models
+{
+    public interface ItemRepository
+    {
+        IEnumerable<Item> GetAllItems();
+        void CreateNewItem(Item item);
+    }
+}
