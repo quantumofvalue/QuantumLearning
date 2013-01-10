bbb536841a974982c0e92b65268ab403d55eafa0
Marcin Czenko <marcin@everydayproductive.com>
2013-01-10 02:24:18 +0100

    Walking Skeleton Scenario.

---
+++ ASP_NET_MVC/Models/ItemDatabaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_NET_MVC.Models
+{
+    public class ItemDatabaseRepository : ItemRepository
+    {
+        private ItemDBContext _db = new ItemDBContext();
+
+        public IEnumerable<Item> GetAllItems()
+        {
+            return _db.Items.ToList();
+        }
+
+        public void CreateNewItem(Item item)
+        {
+            _db.Items.Add(item);
+            _db.SaveChanges();
+        }
+    }
+}
