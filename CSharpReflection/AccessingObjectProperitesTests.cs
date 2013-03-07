f294120b7f92178b91e1ed8d291c9e8fded857ac
Marcin Czenko <marcin@everydayproductive.com>
2013-03-07 16:25:44 +0100

    Added projects for testing reflection in C# and demonstration of a factory pattern.

---
+++ CSharpReflection/AccessingObjectProperitesTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpReflection
+{
+    public class Source
+    {
+        public Dictionary<string, object> table;
+
+        public Source()
+        {
+            table = new Dictionary<string, object>();
+            table.Add("Attribute1", "value1");
+            table.Add("Attribute2", 234);
+        }
+    }
+    
+    public class Destination
+    {
+        public string Attribute1 { get; set; }
+        public int Attribute2 { get; set; }
+    }
+
+    [TestClass]
+    public class AccessingObjectProperitesTests
+    {
+        [TestMethod]
+        public void ReadingObjectsProperties()
+        {
+
+            Destination dest = new Destination();
+            Source source = new Source();            
+
+            var properties = dest.GetType().GetProperties().OrderBy(x => x.Name).Select(x => x.Name);
+
+            System.Console.WriteLine("BEFORE:");
+            foreach(var property in properties)
+            {
+                System.Console.WriteLine("{0}:{1}", property, dest.GetType().GetProperty(property).GetValue(dest));
+
+                dest.GetType().GetProperty(property).SetValue(dest, source.table[property.ToString()]);
+            }
+
+            System.Console.WriteLine("AFTER:");
+            foreach (var property in properties)
+            {                
+                System.Console.WriteLine("{0}:{1}",property,dest.GetType().GetProperty(property).GetValue(dest));
+            }           
+        }
+    }
+}
---
+++ FactoryTests/FactoryTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactoryTests
+{
+    public interface IClassA
+    {
+    }
+
+    public class ClassA : IClassA
+    { 
+    }
+
+    public class ClassB : IClassA
+    {
+    }
+
+    public class Factory
+    {
+        static public IClassA create(String className)
+        {
+            if ("ClassA" == className)
+            {
+                return new ClassA();
+            }
+            else if ("ClassB" == className)
+            {
+                return new ClassB();
+            }
+
+            return null;
+        }
+
+        static public IClassA createClassA()
+        {
+            return new ClassA();
+        }
+
+        static public IClassA createClassB()
+        {
+            return new ClassB();
+        }
+    }
+
+    [TestClass]
+    public class FactoryTests
+    {
+        [TestMethod]
+        public void TestFactoryCreatesInstanceOfClassA()
+        {         
+            Assert.IsInstanceOfType(Factory.create("ClassA"), typeof(ClassA));
+        }
+
+        [TestMethod]
+        public void TestFactoryCreatesInstanceOfClassB()
+        {
+            Assert.IsInstanceOfType(Factory.create("ClassB"), typeof(ClassB));
+        }
+    }
+}
