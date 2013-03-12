950e213f7741251a0754c2b26aaaa9b8a44ff395
Marcin Czenko <marcin@everydayproductive.com>
2013-03-12 20:20:58 +0100

    Testing Cradiator: Photos

--- ASP_NET_MVC.AcceptanceTests/StepDefinitions/WalkingSkeletonSteps.cs
+++ ASP_NET_MVC.AcceptanceTests/StepDefinitions/WalkingSkeletonSteps.cs
@@ -59,7 +59,7 @@ namespace ASP_NET_MVC.AcceptanceTests.StepDefinitions
         [When(@"I enter ""(.*)"" into the ""(.*)"" field")]
         public void WhenIEnterIntoTheField(string fieldText, string fieldId)
         {
-            Type(fieldId, "HELLO");
+            Type(fieldId, "HELLO2");
         }
         
         [When(@"I click ""(.*)""")]
