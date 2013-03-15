425f920ed3d45364dd570556b78441a9868d9bdf
Marcin Czenko <marcin@everydayproductive.com>
2013-03-15 01:36:24 +0100

    Now Integration Tests also test models from ASP_NET_MVC

--- APS_NET_MVC.IntegrationTests/Database/LINQExercises.cs
+++ APS_NET_MVC.IntegrationTests/Database/LINQExercises.cs
@@ -3,13 +3,15 @@ using System.IO;
 using System.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using ASP_NET_MVC.Models;
+
 namespace APS_NET_MVC.IntegrationTests
 {
     [TestClass]
     public class LINQExercises
     {
         [TestMethod]
-        public void AddingItemsToDatabase()
+        public void AddingItemsToTestDatabase()
         {
             AppDomain.CurrentDomain.SetData("DataDirectory", Path.Combine(new string[] {
                 System.Environment.GetEnvironmentVariable("QUANTUM"),
@@ -19,6 +21,20 @@ namespace APS_NET_MVC.IntegrationTests
 
             TestItem item = new TestItem { Text = "Example Text" };
             db.TestItems.Add(item);
+            db.SaveChanges();
+        }
+
+        [TestMethod]
+        public void AddingItemsToDatabase()
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", Path.Combine(new string[] {
+                System.Environment.GetEnvironmentVariable("QUANTUM"),
+                ConfigurationManager.AppSettings["DataDirectory"] }));
+
+            ItemDBContext db = new ItemDBContext();
+
+            Item item = new Item { ItemText = "Example Item Text" };
+            db.Items.Add(item);
             db.SaveChanges();
         }
     }
